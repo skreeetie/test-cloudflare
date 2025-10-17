ce84593526cc14cb08ce5f9f5df01bf674610207
Artem <skreeetie@gmail.com>
2025-10-17 03:38:50 +0300

    idk

--- src/shared/api/matchGamesApi.ts
+++ src/shared/api/matchGamesApi.ts
@@ -12,7 +12,7 @@ type MatchGameOptional = {
   right?: string;
 };
 
-const API_BASE_URL = import.meta.env.API_BASE_URL || "";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "";
 
 export const matchGamesApi = createApi({
   reducerPath: "matchGames",
